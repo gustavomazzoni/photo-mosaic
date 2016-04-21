cbd06eff1459b944cafab24ea34858c86d9e86d8
Gustavo Mazzoni <gustavo.mazzoni@gmail.com>
2016-04-21 17:24:33 -0300

    Changed server config port to work in heroku.

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ http.createServer(function (req, res) {
   res.writeHead(404, {'Content-Type': 'text/plain'});
   res.write('404 Not Found\n');
   res.end();
-}).listen(port, 'localhost'); // 8765, 'localhost');
+}).listen(port); // 8765, 'localhost');
 
-console.log('mosaic server running on port 8765');
+console.log('mosaic server running on port '+port);
