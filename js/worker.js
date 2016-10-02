748e6367d122868ef0821139478e45c90e96c536
Gustavo Mazzoni <gustavo.mazzoni@gmail.com>
2016-10-02 13:27:24 -0300

    Remove debug

--- js/worker.js
+++ js/worker.js
@@ -2,8 +2,7 @@
 importScripts('image-helper.js');
 
 this.onmessage = function(event) {
-    var startTime = Date.now(),
-        rgb = ImageHelper.getAverageRGB(event.data.imageData),
+    var rgb = ImageHelper.getAverageRGB(event.data.imageData),
         // Convert color in rgb to hex
         colorHex = ImageHelper.rgbToHex(rgb.r, rgb.g, rgb.b);
     
@@ -11,6 +10,4 @@ this.onmessage = function(event) {
 		index: event.data.index, 
 		result: {imageData: event.data.imageData, colorHex: colorHex, index: event.data.tileIndex}
 	});
-    var endTime = Date.now();
-    console.log('[Worker] Execution time (ms): ', (endTime - startTime));
 }
